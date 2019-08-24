ad858cdaa356b0b67918832f65960294a1dda9bf
willyr2501 <54484201+willyr2501@users.noreply.github.com>
2019-08-24 14:30:46 -0700

    Update main.cpp

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -8,17 +8,17 @@ int main()
 
   // Get the number of hours worked.
   std::cout << "How many candy bars did you sell? ";
-  std::cin >> 135;
+  std::cin >> hours;
 
   // Get the hourly pay rate.
   std::cout << "How much does the organization earn for each bar sold? ";
-  std::cin >> 0.45;
+  std::cin >> rate;
 
   // Calculate the pay.
   pay = hours * rate;
 
   // Display the pay.
-  std::cout << "Congratulations! You have earned $60.75 for the organization!" << pay << std::endl;
+  std::cout << "You have earned $" << pay << std::endl;
 
   return 0;
 }
